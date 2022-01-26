af972265e5c386a9790e9e6ccbedf276da790a71
yulia <julka.getman@gmail.com>
2022-01-26 16:11:05 +0200

    changed the description of the test task_4 and change let on const

--- Task_4/index.js
+++ Task_4/index.js
@@ -1,6 +1,5 @@
 export function getOperands(str) {
-    let arr = str.split(' ');
-    let myArr = arr.splice(1,1);
-    let res = arr;
-    return 'a:'+' '+res[0]+','+' '+'b:'+' '+res[1];
+    const arr = str.split(' ');
+    const myArr = arr.splice(1,1);
+    return 'a:'+' '+arr[0]+','+' '+'b:'+' '+arr[1];
 }
--- Task_4/index.spec.js
+++ Task_4/index.spec.js
@@ -1,16 +1,16 @@
 import {getOperands} from './index.js'
 
-describe("Tests", () => {
-  it('should return operands', () => {
+describe("getOperands", () => {
+  it('should returns operands in the next format: "a: <first_operand>, b: <second_operand>"', () => {
     expect(getOperands('1 + 2')).toBe('a: 1, b: 2');
   });
-  it('should return operands', () => {
+  it('should returns operands in the next format: "a: <first_operand>, b: <second_operand>"', () => {
     expect(getOperands('1.2 * -3.4')).toBe('a: 1.2, b: -3.4');
   });
-  it('should return operands', () => {
+  it('should returns operands in the next format: "a: <first_operand>, b: <second_operand>"', () => {
     expect(getOperands('1 - 2')).toBe('a: 1, b: 2');
   });
-  it('should return operands', () => {
+  it('should returns operands in the next format: "a: <first_operand>, b: <second_operand>"', () => {
     expect(getOperands('1 - -2')).toBe('a: 1, b: -2');
   });
 });
