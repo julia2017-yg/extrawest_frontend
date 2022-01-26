891a9aafaaabce6e7a1cb647355b2861186baabe
yulia <julka.getman@gmail.com>
2022-01-26 15:54:55 +0200

    changed the description of the test task_3

--- Task_3/index.spec.js
+++ Task_3/index.spec.js
@@ -1,7 +1,7 @@
 import {createPhoneNumber} from './index.js'
 
-describe("Tests", () => {
-  it('should return on tel number', () => {
+describe("createPhoneNumber", () => {
+  it('should returns a string of those numbers in the form of a phone number', () => {
     expect(createPhoneNumber([1,2,3,4,5,6,7,8,9,0])).toBe("(123) 456-7890");
   });
 });
