1e498aceb22777c11d61af313e3b19cb0fafdd51
julia <julka.getman@gmail.com>
2022-01-27 19:31:36 +0200

    change condition

--- Task_6/index.js
+++ Task_6/index.js
@@ -2,9 +2,6 @@ export function isPalindrome(str){
   const regEx = /[\W_]/g;
   const lowStr = str.toLowerCase().replace(regEx, '');
   const reverseStr = lowStr.split('').reverse().join('');
-    if(reverseStr === lowStr){
-      return true;
-    }else {
-      return false
-    }
+  return reverseStr === lowStr ? true : false;
+    
 }
