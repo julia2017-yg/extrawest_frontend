fe1bb68fdbf9a37b4ab0f9ab1c861a79d1332607
yulia <julka.getman@gmail.com>
2022-01-27 15:56:23 +0200

    chage description in test task_6

--- Task_6/index.spec.js
+++ Task_6/index.spec.js
@@ -1,13 +1,13 @@
 import {isPalindrome} from './index.js'
 
 describe("isPalindrome", () => {
-  it('should returns a string, determine if it is a palindromes', () => {
+  it('should return true when pass string Eva, can I see bees in a cave?', () => {
     expect(isPalindrome('Eva, can I see bees in a cave?')).toBe(true);
   });
-  it('should returns a string, determine if it is a palindrome', () => {
+  it('should return false when pass string race a car', () => {
     expect(isPalindrome('race a car')).toBe(false);
   });
-  it('should returns a string, determine if it is a palindrome', () => {
+  it('should return true when pass string Was it a cat I saw?', () => {
     expect(isPalindrome('Was it a cat I saw?')).toBe(true);
   });
 });
