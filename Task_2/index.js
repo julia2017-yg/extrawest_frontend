acb5432bf5283f1a740fd5e18b6c8814e2523ae3
yulia <julka.getman@gmail.com>
2022-01-27 11:14:32 +0200

    rename let om const

--- Task_2/index.js
+++ Task_2/index.js
@@ -2,8 +2,8 @@ export function stringMerge(str1,str2,str3) {
 
   const strNew1 = str1.substring(0,str1.indexOf(str3)+1)
   const from = str2.search(str3)+1;
-  let to = str2.length;
-  let res = strNew1+str2.substring(to,from);
+  const to = str2.length;
+  const res = strNew1+str2.substring(to,from);
     return res;
     
 }
