726db52010d85ded2c12cb43d5ca89d3395a68a9
julia <julka.getman@gmail.com>
2022-01-28 21:37:50 +0200

    change solution task_9

--- Task_9/index.js
+++ Task_9/index.js
@@ -1,4 +1,4 @@
 export function vowelCount(str) { 
-  const regExVowel = str.match(/[aeiou]/gi);
+  const regExVowel = str.match(/[aeiou]/g);
   return regExVowel === null ? 0 : regExVowel.length;
 }
