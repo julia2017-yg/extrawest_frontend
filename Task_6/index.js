557c9ff1d4389f0ec7f8590458d40f07e8c16963
julia <julka.getman@gmail.com>
2022-01-28 16:41:28 +0200

    delete condition

--- Task_6/index.js
+++ Task_6/index.js
@@ -2,7 +2,7 @@ export function isPalindrome(str){
   const regEx = /[\W_]/g;
   const lowStr = str.toLowerCase().replace(regEx, '');
   const reverseStr = lowStr.split('').reverse().join('');
-  
-  return reverseStr === lowStr ? true : false;
+
+  return reverseStr === lowStr;
     
 }
