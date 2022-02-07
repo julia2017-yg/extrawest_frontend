7f1672d51e8b9ec105ed13ce3a97f90a7ab2ce55
julia <julka.getman@gmail.com>
2022-02-07 16:47:28 +0200

    change solution task 7

--- Lecture_2/Task_7/index.js
+++ Lecture_2/Task_7/index.js
@@ -1,13 +1,13 @@
 export const invertColor = hex => {
   const hexArray = hex.toLowerCase().replace('#','').split('');
   const colorsArray = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
- 
+  const reverseArray = [...colorsArray].reverse();
   let inverseColor = '#';
   
   hexArray.forEach(element => {
     const index = colorsArray.indexOf(element);
 
-    inverseColor += colorsArray.reverse()[index];
+    inverseColor += reverseArray[index];
   });
 
   return inverseColor;
